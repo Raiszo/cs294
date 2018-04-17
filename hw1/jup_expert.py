e1fb35259e832b63735bbb6851d41bf476d0bec1
Raiszo <E05.sek@gmail.com>
2018-04-16 20:05:39 -0500

    :dagger: worried

---
+++ hw1/jup_expert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Code to load an expert policy and generate roll-out data for behavioral cloning.
+Example usage:
+    python run_expert.py experts/Humanoid-v1.pkl Humanoid-v1 --render \
+            --num_rollouts 20
+
+Author of this script and included expert policies: Jonathan Ho (hoj@openai.com)
+"""
+
+import pickle
+import tensorflow as tf
+import numpy as np
+import tf_util
+import gym
+import load_policy
+
+from keras.models import Sequential
+from keras.layers import Dense
+
